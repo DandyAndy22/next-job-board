205d383be0c0aa19e71b8b6354ee3ee298022a90
Andras Mihaly <andras@tripleseat.com>
2024-06-05 15:31:47 -0700

    Broken but work in progress
    Setting up navbar and popup menu for navigation items

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+'use client'
+import NavBar from "@/components/NavBar";
 
 export default function Home() {
+  
+
   return (
     <main>
+      <NavBar />
       Job Board, all done!
     </main>
   );
