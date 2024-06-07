3177f23f58a299764a49697b3515606aed86db43
Andras Mihaly <andras@tripleseat.com>
2024-06-07 12:41:14 -0700

    Add Firebase & Firebase config

---
+++ src/app/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app"
+import { getAuth } from "firebase/auth"
+import { getFirestore } from "firebase/firestore"
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: process.env.firebaseApiKey,
+    authDomain: process.env.authDomain,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId
+  };
+  
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig)
+  export const db = getFirestore(app)
+  export const auth = getAuth()
