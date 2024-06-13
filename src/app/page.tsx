5cf045ee2a4146aa6a4ebde3d8bd98c61448fdd4
Andras Mihaly <andras@tripleseat.com>
2024-06-13 16:35:23 -0700

    Home Page Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 import NavBar from "@/components/NavBar";
+import { Box, Typography } from "@mui/material";
 
 export default function Home() {
   
@@ -7,7 +8,10 @@ export default function Home() {
   return (
     <main>
       <NavBar />
-      Home Page! 
+      <Box sx={{ margin: "50px" }}>
+        <Typography variant="h3">Welcome,</Typography>
+        <Typography variant="h6">Give our Job Board a try!</Typography>
+      </Box>
     </main>
   );
 }
